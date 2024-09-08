02cd476ecaa7212b9398dc809876f2250763467b
Amir Adel <amiradel1234567@gmail.com>
2024-09-08 12:13:43 +0300

    fixing singin and sign up routes

--- online-learning-platform-frontend/src/pages/HomePage.js
+++ online-learning-platform-frontend/src/pages/HomePage.js
@@ -6,8 +6,8 @@ function HomePage() {
       <h1 className='text-4xl'>Welcome to the Online Learning Platform</h1>
       <div>
         <ul className='flex  items-center gap-8 mt-6 text-primary text-xl '>
-          <li className='px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'><Link to={"/signin"}>Sign In</Link></li>
-          <li className='px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'><Link to={"/signup"}>Sign Up</Link></li>
+          <li className='px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'><Link to={"/sign-in"}>Sign In</Link></li>
+          <li className='px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'><Link to={"/sign-up"}>Sign Up</Link></li>
           <li className='px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'><Link to={"/courses"}>Courses</Link></li>
           <li className='px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'><Link to={"/profile"}>Profile</Link></li>
           <li className='px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'><Link to={"/about"}>About</Link></li>
