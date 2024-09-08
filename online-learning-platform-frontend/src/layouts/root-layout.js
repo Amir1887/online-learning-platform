3c679600a12b20c6d6a64e49ff39581e886574b3
Amir Adel <amiradel1234567@gmail.com>
2024-09-08 12:11:24 +0300

    fixing position of footer

--- online-learning-platform-frontend/src/layouts/root-layout.js
+++ online-learning-platform-frontend/src/layouts/root-layout.js
@@ -1,16 +1,17 @@
-import { Link, Outlet, useNavigate } from 'react-router-dom'
-import { ClerkProvider, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { ClerkProvider, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+
 const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error('Missing Publishable Key');
 }
 
 //This component acts as the root layout
 export default function RootLayout() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <ClerkProvider
@@ -18,23 +19,25 @@ export default function RootLayout() {
       routerReplace={(to) => navigate(to, { replace: true })}
       publishableKey={PUBLISHABLE_KEY}
     >
-      <Header >
-        <div>
+      <div className="flex flex-col min-h-screen">
+        <Header className="flex-none" >
           <div>
-            <p>Clerk + React + React Router App</p>
+            <div>
+              <p>Clerk + React + React Router App</p>
+            </div>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <Link to="/sign-in">Sign In</Link>
+            </SignedOut>
           </div>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <SignedOut>
-            <Link to="/sign-in">Sign In</Link>
-          </SignedOut>
-        </div>
-      </Header>
-      <main>
-        <Outlet />
-      </main>
-      <Footer /> 
+        </Header>
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        <Footer className="flex-none" />
+      </div>
     </ClerkProvider>
-  )
+  );
 }
