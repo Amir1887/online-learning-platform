2b0491858ba105821a2ea62da7ee19027881e48f
Amir Adel <amiradel1234567@gmail.com>
2024-09-10 22:17:59 +0300

    modifying query to get lessons from database

--- online-learning-backend/routes/singleCourseRoutes.js
+++ online-learning-backend/routes/singleCourseRoutes.js
@@ -18,14 +18,20 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    // We query the Course and join the Author table to fetch the author's name, image, and total reviews alongside the course data.                                                       
     const result = await pool.query(
-      `SELECT c.*, a.name as author_name, a.image as author_image, a."totalReviews"  as author_totalReviews 
+      `SELECT c.*, a.name as author_name, a.image as author_image, a."totalReviews" as author_totalReviews, 
+              json_agg(json_build_object('lesson_id', l.id, 'title', l.title, 'content', l.content)) as lessons 
        FROM "Course" c 
        JOIN "Author" a ON c."authorId" = a.id 
-       WHERE c.id = $1`,
-       [id]);
+       LEFT JOIN "Lesson" l ON l."courseId" = c.id 
+       WHERE c.id = $1
+       GROUP BY c.id, a.id`,
+       [id]
+    );
+    //json_build_object: This creates a JSON object for each lesson, containing the lesson's id, title, and content.
+    // json_agg: This aggregates the Lesson data for each course into a JSON array, making it easier to handle in the frontend.
     console.log("query result", result);
+    
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'Course not found' });
     }
