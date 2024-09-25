efc48a3b77b9c6df4133990203194aaea4419420
Amir Adel <amiradel1234567@gmail.com>
2024-09-25 16:44:31 +0300

    making a route at the backend which insert assignment data sent from frontend by author > insert it into Assignment table.* 1st we coonect to db using Pool * getting { courseId, lessonId }, { title,questions } from frontend :they are set in url * then we insert at table

---
+++ online-learning-backend/routes/assignmentRoute.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { Pool } = require('pg');
+const router = express.Router();
+
+// PostgreSQL connection
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'LearningDb',
+  password: 'postgress',
+  port: 5432,
+});
+
+// (`http://localhost:4000/course/${courseId}/lesson/${lessonId}/create-assignment`
+router.post('/course/:courseId/lesson/:lessonId/create-assignment', async (req, res) => {
+    const { courseId, lessonId } = req.params;
+    const { title,questions } = req.body;
+  
+    try {
+      // Store assignment with assignment_data as JSONB
+        const result = await pool.query(
+        `INSERT INTO "Assignment" (title, "courseId", "lessonId", assignments) 
+         VALUES ($1, $2, $3, $4) RETURNING *`,
+       [title, courseId, lessonId, { questions }] );
+    
+       res.status(201).json(result.rows[0]); // Return the newly created assignment
+
+    } catch (error) {
+        console.error('Error creating assignment:', error);
+        res.status(500).json({ error: 'Error creating assignment' });
+    }
+});
+
+module.exports = router;
