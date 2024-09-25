b1ab24fc4e38774c8edcfccb4c05db12f3c7ecaa
Amir Adel <amiradel1234567@gmail.com>
2024-09-25 10:13:38 +0300

    making comments in assignment of form for clarity

--- online-learning-platform-frontend/src/pages/AuthorAssignmentForm .js
+++ online-learning-platform-frontend/src/pages/AuthorAssignmentForm .js
@@ -4,21 +4,32 @@ import { useParams } from 'react-router-dom';
 
 const AuthorAssignmentForm = () => {
     const {courseId, lessonId} = useParams();
+
+// Initializes state with an array containing one question object. Each question has:
+    // An empty question string.
+    // An array of choices (4 empty strings).
+    // An empty correctAnswer string.
   const [questions, setQuestions] = useState([
     { question: '', choices: ['', '', '', ''], correctAnswer: '' }
   ]);
 
+  //adds a new empty question object to the questions state (adding new qs to assignment)
   const addQuestion = () => {
     setQuestions([...questions, { question: '', choices: ['', '', '', ''], correctAnswer: '' }]);
   };
 
+
+  //This function updates the state based on user input:
+          //1.It takes an index of the question, the field being updated, and the new value.
+          //2.Creates a copy of the current questions state.
+          //3.Updates the specific property (question text, choice, or correct answer) of the selected question.
   const handleQuestionChange = (index, field, value) => {
-    const updatedQuestions = [...questions];
+    const updatedQuestions = [...questions]; //Creates a copy
     if (field === 'question') {
       updatedQuestions[index].question = value;
     } else if (field.startsWith('choice')) {
       const choiceIndex = parseInt(field.split('choice')[1]);
-      updatedQuestions[index].choices[choiceIndex] = value;
+      updatedQuestions[index].choices[choiceIndex] = value; // index > index of qs but choiceIndex is index of choice 
     } else if (field === 'correctAnswer') {
       updatedQuestions[index].correctAnswer = value;
     }
@@ -27,7 +38,7 @@ const AuthorAssignmentForm = () => {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post(`http://localhost:4000/course/${courseId}/create-assignment`, { questions });
+      const response = await axios.post(`http://localhost:4000/course/${courseId}/lesson/${lessonId}/create-assignment`, { questions });
       console.log(response.data);
     } catch (error) {
       console.error('Error creating assignment', error);
