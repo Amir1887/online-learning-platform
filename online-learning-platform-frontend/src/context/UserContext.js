9a5e04389d2b40dd58a2399cce6cc3f094826844
Amir Adel <amiradel1234567@gmail.com>
2024-10-02 00:43:53 +0300

    making separate state for each property of user to access them separately as needed

--- online-learning-platform-frontend/src/context/UserContext.js
+++ online-learning-platform-frontend/src/context/UserContext.js
@@ -8,10 +8,13 @@ const UsereContext = createContext();
 
 // Create a provider component  
 export const UsereProvider = ({ children }) => {  
-    const [userIdFromDb, setuserIdFromDb] =useState(null);
+    const [userIdFromDb, setUserIdFromDb] =useState(null);
+    const [userEmail, setUserEmail] =useState(null);
+    const [userName, setUserName] =useState(null);
+    const [userCreationTime, setUserCreationTime] =useState(null);
+    const [userUpdating, setUserUpdating] =useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-    const { courseId, lessonId, assignmentId } = useParams();
     const { userId } = useAuth(); 
       
     useEffect(() => {
@@ -19,8 +22,13 @@ export const UsereProvider = ({ children }) => {
       const fetchUserId = async () => {
         if (userId && isMounted) {
           try {
-            const res = await axios.get(`http://localhost:4000/user/find-id-from-db/${userId}`, {params : {courseId, lessonId, assignmentId}});
-            setuserIdFromDb(res.data);
+            const res = await axios.get(`http://localhost:4000/user/find-id-from-db/${userId}`);
+            console.log("total data of user:",res)
+            setUserIdFromDb(res.data.LoggedInUserId);
+            setUserEmail(res.data.LoggedInUserEmail);
+            setUserName(res.data.LoggedInUserName);
+            setUserCreationTime(res.data.LoggedInUserCreatedAt);
+            setUserUpdating(res.data.LoggedInUserUpdatedAt);
             setIsLoading(false);
           } catch (err) {
             console.error(err); // This helps with debugging
@@ -37,7 +45,7 @@ export const UsereProvider = ({ children }) => {
       };
     }, [userId]);
     return (  
-      <UsereContext.Provider value={{userIdFromDb, isLoading, error}}>  
+      <UsereContext.Provider value={{userIdFromDb, userEmail, userName, userCreationTime, userUpdating, isLoading, error}}>  
         {children}  
       </UsereContext.Provider>  
     );  
