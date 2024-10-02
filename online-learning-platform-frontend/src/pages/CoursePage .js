0a56e987461da836d69c34826a636fb8b04a1d3d
Amir Adel <amiradel1234567@gmail.com>
2024-10-02 22:19:57 +0300

    implementing showing only the first three assignments initially, with a (Show More) button to reveal the rest of the assignments , I modified the course.assignmetData.map() function to limit the displayed assignments to three. Then, conditionally render the remaining assignments when the (Show More) button is clicke

--- online-learning-platform-frontend/src/pages/CoursePage .js
+++ online-learning-platform-frontend/src/pages/CoursePage .js
@@ -17,7 +17,9 @@ const CoursePage = () => {
     // Determine whether to show all lessons or just the first 3  
   const lessonsToShow = course && course.lessons ? (showAllLessons ? course.lessons : course.lessons.slice(0, 3)) : [];
  
-
+  //to conditionally render asignment
+  const [showAllAssignments, setShowAllAssignments] =useState(false);
+  const assignmentsToShow = course && course.assignmetData ? (showAllAssignments ? course.assignmetData: course.assignmetData.slice(0,3)) :[];
 
 
 
@@ -157,13 +159,23 @@ if (loading) return <div>Loading...</div>;
 <div className="my-8 ml-7">
   {course.assignmetData && course.assignmetData.length > 0 ? (
     <div className="space-y-4">
-      {course.assignmetData.map((assignment, index) => (
+      {assignmentsToShow.map((assignment, index) => (
         <Link to={`/dashboard/course/${assignment.course_id}/lesson/${assignment.lesson_id}/assignments-to-lesson/${assignment.assignment_id}`} key={index}  className="block border border-gray-200 shadow-md rounded-lg p-4 transition hover:shadow-lg hover:border-blue-400">
           <h3 className="font-semibold text-xl text-blue-600">Assignment: {assignment.assignment_title}</h3>
           <p className="text-gray-600 mt-2">created At: {new Date(assignment.createdAt).toLocaleDateString()}</p>
           <p className="text-gray-600 mt-2">On lesson: {assignment.lesson_title}</p>
         </Link>
       ))}
+
+        {/* Show More/Show Less Button */}
+      {course.assignmetData.length > 3 && (
+        <button
+            onClick={() => setShowAllAssignments(!showAllAssignments)}
+            className="text-blue-600 hover:underline mt-4 block"
+            >
+            {showAllLessons ? 'Show Less' : 'Show More'}
+       </button>
+      )}
     </div>
   ) : (
     <p className="text-gray-500">No assignments for this course yet.</p>
